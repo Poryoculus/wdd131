4fae17cd2c25ec8e327a4c46e2fb9a8fad1ae730
Poryoculus <rodrigopachecojor@gmail.com>
2024-11-14 20:34:24 -0300

    1st commit

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const year = document.querySelector("#currentyear")
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+
+let lastModified = document.querySelector("#lastModified");
+let lastmodification = new Date(document.lastModified);
+lastModified.innerHTML = `Last modification: <span class="highlight"> ${lastmodification}`;
