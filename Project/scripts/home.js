cbbb20e4b5d7ec0e057758a51446db84759b6e3f
Poryoculus <rodrigopachecojor@gmail.com>
2025-04-17 15:25:31 -0400

    final details

--- Project/scripts/home.js
+++ Project/scripts/home.js
@@ -18,3 +18,33 @@ window.addEventListener("resize", function () {
   var x = document.querySelector(".navigation nav");
   x.style.display = window.innerWidth > 600 ? "flex" : "none";
 });
+
+// Welcome message
+
+const welcomeMessage = document.querySelector("#message");
+const now = Date.now();
+const lastVisit = localStorage.getItem("lastvisit");
+
+let message = "";
+
+if (lastVisit) {
+  const lastTime = Number(lastVisit);
+  const diffTime = now - lastTime;
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); // Convert to days
+
+  if (diffDays < 1) {
+    message = "Back so soon! Awesome!";
+  } else if (diffDays === 1) {
+    message = "You last visited 1 day ago.";
+  } else {
+    message = `Your last visit was ${diffDays} days ago.`;
+  }
+
+  console.log(diffDays);
+} else {
+  message = "Welcome! Let us know if you have any questions.";
+}
+
+localStorage.setItem("lastvisit", now);
+
+welcomeMessage.textContent = message;
